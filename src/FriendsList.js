28c25dd48c5cc9fdc9a9cfb0c8dc6d38d9580982
Aaron Schuster <aaronjschuster@gmail.com>
2023-02-21 18:55:24 -0500

    add age to friends list component

--- src/FriendsList.js
+++ src/FriendsList.js
@@ -2,23 +2,20 @@ import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import axiosWithAuth from "./axiosWithAuth";
 
-
 function FriendsList() {
 
     const [friends, setFriends] = useState([]);
 
     useEffect(() => {
         axiosWithAuth().get("/friends")
-            .then(res => {
-                setFriends(res.data);
-            })
+            .then(res => setFriends(res.data))
             .catch(err => console.error(err));
     }, [])
     if (localStorage.getItem("token")) {
         return (
             <div>
                 {friends.map(friend => (
-                    <p key={friend.id}> - {friend.name} - {friend.email} </p>
+                    <p key={friend.id}>{friend.name} - Age: {friend.age} - {friend.email} </p>
                 ))}
             </div>
         )
