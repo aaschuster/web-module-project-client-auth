84599d759dd3201eee0d284991424f1708fbc2a8
Aaron Schuster <aaronjschuster@gmail.com>
2023-02-21 18:31:39 -0500

    add age input on add friend form. also reset form to empty when submitting

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -6,7 +6,8 @@ import axiosWithAuth from "./axiosWithAuth";
 function AddFriend() {
     const initForm = {
         name: "",
-        email: ""
+        email: "",
+        age: ""
     }
 
     const [form, setForm] = useState(initForm);
@@ -20,27 +21,30 @@ function AddFriend() {
 
     function onSubmit (e) {
         e.preventDefault();
-        console.log(form.name);
-        console.log(form.email);
         axiosWithAuth().post("/friends", 
             {
                 id: new Date().getTime(),
                 name: form.name,
-                email: form.email
+                email: form.email,
+                age: form.age
             }
         )
         .then( res => {
             console.log(res);
         }).catch(err => console.error(err));
+        setForm(initForm);
     }
     if (localStorage.getItem("token")) {
         return (
             <form onSubmit={onSubmit}>
                 <label> Name
-                    <input type="text" name="name" placeholder="Name..." onChange={onChange}/>
+                    <input type="text" name="name" placeholder="Name..." onChange={onChange} value={form.name}/>
+                </label>
+                <label> Age
+                    <input type="text" name="age" placeholder="Age..." onChange={onChange} value={form.age}/>
                 </label>
                 <label> Email
-                    <input type="email" name="email" placeholder="Email..." onChange={onChange}/>
+                    <input type="email" name="email" placeholder="Email..." onChange={onChange} value={form.email}/>
                 </label>
                 <button>Submit</button>
             </form>
